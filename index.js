e38f9f422c0550b772b2cf7437520d99585a5864
Andrew Malta <andrew.malta@yale.edu>
2015-06-26 16:37:41 -0700

    refactored module to conform to common-blockstore as well

--- index.js
+++ index.js
@@ -1,152 +1,18 @@
-var request = require('request');
+var Addresses = require('./lib/addresses.js');
+var Transactions = require('./lib/transactions.js');
+var Blocks = require('./lib/blocks.js');
 
-function calculateFee(inputs, outputs){
-  var totalIn = 0;
-  var totalOut = 0;
-  for(var i = 0; i < inputs.length; i++){
-    totalIn += inputs[i].output_value;
-  }
 
-  for(var i = 0; i < outputs.length; i++){
-    totalOut += outputs[i].value;
-  }
-  return(totalIn - totalOut);
+//config variables for the module. (only network for now)
+var config = {};
+
+function BlockCypher(opts) {
+  if (!(this instanceof BlockCypher)) return new BlockCypher(opts);
+  config.network = opts.network;
 }
 
-//returns the correct url endpoint based on network
-function getBaseURL(network){
-  var baseURL;
-  if (network === "testnet"){
-    baseURL = "https://api.blockcypher.com/v1/btc/test3";
-  } else {
-    baseURL = "https://api.blockcypher.com/v1/btc/main";
-  }
-  return baseURL;
-}
+BlockCypher.prototype.Addresses = Addresses(config);
+BlockCypher.prototype.Transactions = Transactions(config);
+BlockCypher.prototype.Blocks = Blocks(config);
 
-//abstracted json get request caller method.
-function getFromURL(url, callback){
-  request.get(url, function (err, response, body) {
-    if (err) {
-      console.log("error fetching info from blockcypher " + err);
-      callback(err, null);
-    } else {
-      try {
-        callback(false, JSON.parse(body));
-      } 
-      catch(err) {
-        callback(err, null);
-      }
-    }
-  });
-}
-
-//returns wallet info in standard format to callback.
-function getWalletInfo(options, callback){
-  var response = {};
-  var url = getBaseURL(options.network) + "/addrs/" + options.address;
-  getFromURL(url, function (err, resp){
-    if(err){
-      callback(err, null);
-    }
-    else{
-      response.balance = resp.balance;
-      response.totalReceived = resp.total_received;
-      response.totalSent = resp.total_sent;
-      response.txCount = resp.n_tx;
-      callback(false, response);
-    }
-  });
-}
-
-//returns unspents for options.address in standard format to callback.
-function getUnspentOutputs(options, callback){
-  var url = getBaseURL(options.network) + "/addrs/" + options.address + "?unspentOnly=true";
-  var response = {"unspents" : []};
-  getFromURL(url, function (err, resp){
-    if(err){
-      callback(err, null);
-    } else {
-      resp.txrefs.forEach(function (unspent){
-        var utxo = {};
-        utxo.txid = unspent.tx_hash;
-        utxo.vout = unspent.tx_output_n;
-        utxo.address = options.address;
-        utxo.scriptPubKey = "not provided";
-        utxo.amount = unspent.value;
-        utxo.confirmations = unspent.confirmations;
-        
-        response.unspents.push(utxo);
-      }); 
-      callback(err, response);
-    }
-  });
-}
-
-//returns transaction information for options.txid in standard format to callback.
-function getTransaction(options, callback){
-  var url = getBaseURL(options.network) + "/txs/" + options.txid;
-  var response = {};
-  getFromURL(url, function (err, resp){
-    if(err){
-      callback(err, null);
-    } else {
-      response.hex = "not provided";
-      response.txid = resp.hash;
-      response.version = resp.ver;
-      response.locktime = resp.lock_time;
-      response.fee = calculateFee(resp.inputs, resp.outputs);
-      response.vin = [];
-      response.vout = [];
-      resp.inputs.forEach(function (input){
-        response.vin.push({txid: input.prev_hash, vout: input.output_index, 
-                            scriptSig: {asm: "not provided", hex: input.script}, sequence: input.sequence});
-      });
-      resp.outputs.forEach(function (output){
-        response.vout.push({value: output.value, index: "not provided", spentTxid: "not provided",
-                           scriptPubKey : {asm: "not provided", hex: output.script, reqSigs: output.addresses.length,
-                           type: output.script_type, addresses: output.addresses}});
-      });
-      
-      response.blockhash = resp.block_hash;
-      response.blockindex = "not provided"
-      response.blocktime = new Date(resp.confirmed).getTime();
-      response.confirmations = resp.confirmations;
-      response.timeReceived = new Date(resp.received).getTime();
-
-      callback(err, response);
-    }
-  });
-}
-
-//pushes options.transaction in hex to network and returns the resultant txid to callback.
-function pushTransaction(options, callback){
-  var postBody = {"tx": options.transaction};
-  var url = getBaseURL(options.network) + "/txs/push";
-
-  request({
-      url: url, //URL to hit
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(postBody)
-    }, function(err, response, body){
-      if(err) {
-        console.log("error pushing raw txhex to blockcypher" + err);
-        callback(err, null);
-      } else {
-        try {
-          callback(false, JSON.parse(body).tx.hash);
-        } 
-        catch(err) {
-          callback(err, null);
-        }
-      }
-  });
-}
-
-module.exports.getWalletInfo = getWalletInfo;
-module.exports.getUnspentOutputs = getUnspentOutputs;
-module.exports.getTransaction = getTransaction;
-module.exports.pushTransaction = pushTransaction;
+module.exports = BlockCypher;
---
+++ test.js
@@ -0,0 +1,38 @@
+var blockcypher = require("./index.js")({network: "testnet"});
+
+
+function test(err, resp){
+  if(err){
+    console.log(err);
+  } else {
+    console.log(resp);
+  }
+}
+
+//WARNING, be weary of block cypher's request limit. These tests 
+
+// blockcypher.Addresses.unspents(["mjf6CRReqGSyvbgryjE3fbGjptRRfAL7cg"], test);
+// blockcypher.Addresses.summary(["mjf6CRReqGSyvbgryjE3fbGjptRRfAL7cg"], test);
+// blockcypher.Addresses.transactions(["mjf6CRReqGSyvbgryjE3fbGjptRRfAL7cg"], test);
+
+// blockcypher.Blocks.get(["00000000005df195c304bc89652377f3ef17ed8c71c636e88adecb0bbf20f873"], test);
+// blockcypher.Blocks.latest(test);
+// blockcypher.Blocks.propogate("askfhkajshfdk", test);
+
+// blockcypher.Transactions.get(["940d527cb2f75c2fd3a5edaab29932891f1738d82934ba8f3d9bff4d22ea33f5",
+// //  "0409c167be7f367dbf5ba065b662c971dabfbc431a458af7dfb298f300026b86", "04416cedbdfff7a841263b26d1a049f3baca0384667171c0872a9910bbac3630"], test);
+// blockcypher.Transactions.latest(test);
+blockcypher.Transactions.outputs({"outputs" : [{txid: "940d527cb2f75c2fd3a5edaab29932891f1738d82934ba8f3d9bff4d22ea33f5", vout: 0},
+                                  {txid: "0409c167be7f367dbf5ba065b662c971dabfbc431a458af7dfb298f300026b86", vout: 1},
+                                  {txid: "9375818c85a6712416dac6edd403498180ee9ee0e604bd11ec35beaea384da51", vout: 0}]}, test);
+
+// blockcypher.Transactions.propogate("0100000001268a9ad7bfb2\
+// 1d3c086f0ff28f73a064964aa069ebb69a9e437da85c7e55c7d7000000006b48\
+// 3045022100ee69171016b7dd218491faf6e13f53d40d64f4b40123a2de52560f\
+// eb95de63b902206f23a0919471eaa1e45a0982ed288d374397d30dff541b2dd4\
+// 5a4c3d0041acc0012103a7c1fd1fdec50e1cf3f0cc8cb4378cd8e9a2cee8ca9b\
+// 3118f3db16cbbcf8f326ffffffff0350ac6002000000001976a91456847befbd\
+// 2360df0e35b4e3b77bae48585ae06888ac80969800000000001976a9142b1495\
+// 0b8d31620c6cc923c5408a701b1ec0a02088ac002d3101000000001976a9140d\
+// fc8bafc8419853b34d5e072ad37d1a5159f58488ac00000000" , test);
+// blockcypher.Transactions.status(["9375818c85a6712416dac6edd403498180ee9ee0e604bd11ec35beaea384da51"], test);
