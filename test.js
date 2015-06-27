b851c6f4b5b442ec93c2ce95dd7a16ed23b9eaf1
Andrew Malta <andrew.malta@yale.edu>
2015-06-26 17:13:28 -0700

    changes to interface

--- test.js
+++ test.js
@@ -1,19 +1,20 @@
-var blockcypher = require("./index.js")({network: "testnet"});
+var blockcypher = require("./index.js")({network: "mainnet"});
 
 
 function test(err, resp){
   if(err){
     console.log(err);
   } else {
-    console.log(resp);
+    console.log(JSON.stringify(resp));
   }
 }
 
 //WARNING, be weary of block cypher's request limit. These tests 
 
-// blockcypher.Addresses.unspents(["mjf6CRReqGSyvbgryjE3fbGjptRRfAL7cg"], test);
-// blockcypher.Addresses.summary(["mjf6CRReqGSyvbgryjE3fbGjptRRfAL7cg"], test);
-// blockcypher.Addresses.transactions(["mjf6CRReqGSyvbgryjE3fbGjptRRfAL7cg"], test);
+//blockcypher.Addresses.Unspents({addresses: ["mjf6CRReqGSyvbgryjE3fbGjptRRfAL7cg"]}, test);
+
+//blockcypher.Addresses.Summary({addresses: ["mjf6CRReqGSyvbgryjE3fbGjptRRfAL7cg"]}, test);
+// blockcypher.Addresses.Transactions({addresses: ["1HUTmSsFp9Rg4FYRftp85GGyZFEndZSoeq"]}, test);
 
 // blockcypher.Blocks.get(["00000000005df195c304bc89652377f3ef17ed8c71c636e88adecb0bbf20f873"], test);
 // blockcypher.Blocks.latest(test);
@@ -22,9 +23,9 @@ function test(err, resp){
 // blockcypher.Transactions.get(["940d527cb2f75c2fd3a5edaab29932891f1738d82934ba8f3d9bff4d22ea33f5",
 // //  "0409c167be7f367dbf5ba065b662c971dabfbc431a458af7dfb298f300026b86", "04416cedbdfff7a841263b26d1a049f3baca0384667171c0872a9910bbac3630"], test);
 // blockcypher.Transactions.latest(test);
-blockcypher.Transactions.outputs({"outputs" : [{txid: "940d527cb2f75c2fd3a5edaab29932891f1738d82934ba8f3d9bff4d22ea33f5", vout: 0},
-                                  {txid: "0409c167be7f367dbf5ba065b662c971dabfbc431a458af7dfb298f300026b86", vout: 1},
-                                  {txid: "9375818c85a6712416dac6edd403498180ee9ee0e604bd11ec35beaea384da51", vout: 0}]}, test);
+// blockcypher.Transactions.outputs({"outputs" : [{txid: "940d527cb2f75c2fd3a5edaab29932891f1738d82934ba8f3d9bff4d22ea33f5", vout: 0},
+//                                   {txid: "0409c167be7f367dbf5ba065b662c971dabfbc431a458af7dfb298f300026b86", vout: 1},
+//                                   {txid: "9375818c85a6712416dac6edd403498180ee9ee0e604bd11ec35beaea384da51", vout: 0}]}, test);
 
 // blockcypher.Transactions.propogate("0100000001268a9ad7bfb2\
 // 1d3c086f0ff28f73a064964aa069ebb69a9e437da85c7e55c7d7000000006b48\
