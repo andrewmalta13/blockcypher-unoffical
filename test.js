44a2240c75055d70ee407d77830d78c48888fb03
Andrew Malta <andrew.malta@yale.edu>
2015-06-30 13:45:53 -0700

    type for propagate

--- test.js
+++ test.js
@@ -27,7 +27,7 @@ function test(err, resp){
 // api({network: "mainnet"}).Blocks.Latest(test);
 // api({network: "testnet"}).Blocks.Latest(test);
 
-//api({network: "testnet"}).Blocks.Propogate("askfhkajshfdk", test);
+//api({network: "testnet"}).Blocks.Propagate("askfhkajshfdk", test);
 
 //api({network: "mainnet"}).Blocks.Transactions({"blockids": ["00000000000000000216a936ebc1962e319a51bab8d3eae69335ac940284491d"]}, test);
 //api({network: "testnet"}).Blocks.Transactions({"blockids": ["00000000005df195c304bc89652377f3ef17ed8c71c636e88adecb0bbf20f873"]}, test);
@@ -51,7 +51,7 @@ api({network: "testnet"}).Transactions.Latest(test);
  //api({network: "mainnet"}).Transactions.Status({"txids" : ["9375818c85a6712416dac6edd403498180ee9ee0e604bd11ec35beaea384da51", "SADFSD"]}, test);
  //api({network: "testnet"}).Transactions.Status({"txids" : ["940d527cb2f75c2fd3a5edaab29932891f1738d82934ba8f3d9bff4d22ea33f5", "SADFSD"]}, test);
 
-// api({network: "mainnet"}).Transactions.Propogate("0100000001268a9ad7bfb2\
+// api({network: "mainnet"}).Transactions.Propagate("0100000001268a9ad7bfb2\
 // 1d3c086f0ff28f73a064964aa069ebb69a9e437da85c7e55c7d7000000006b48\
 // 3045022100ee69171016b7dd218491faf6e13f53d40d64f4b40123a2de52560f\
 // eb95de63b902206f23a0919471eaa1e45a0982ed288d374397d30dff541b2dd4\
@@ -61,7 +61,7 @@ api({network: "testnet"}).Transactions.Latest(test);
 // 0b8d31620c6cc923c5408a701b1ec0a02088ac002d3101000000001976a9140d\
 // fc8bafc8419853b34d5e072ad37d1a5159f58488ac00000000" , test);
 
-// api({network: "testnet"}).Transactions.Propogate("0100000001268a9ad7bfb2\
+// api({network: "testnet"}).Transactions.Propagate("0100000001268a9ad7bfb2\
 // 1d3c086f0ff28f73a064964aa069ebb69a9e437da85c7e55c7d7000000006b48\
 // 3045022100ee69171016b7dd218491faf6e13f53d40d64f4b40123a2de52560f\
 // eb95de63b902206f23a0919471eaa1e45a0982ed288d374397d30dff541b2dd4\
